06ebafc5b9966f8c8d44539e1bf7f70353b80dd1
Agustin Buzarquis <agustinbuzar@thinkion.com.ar>
2017-04-25 10:53:57 -0300

    Update BD

--- scaw/src/main/resources/db/sql/insert-data.sql
+++ scaw/src/main/resources/db/sql/insert-data.sql
@@ -1,16 +1,4 @@
-INSERT INTO person (FIRSTNAME, LASTNAME, EMAIL) VALUES ('Cintia', 'Gioia', 'cgioia@unlam.edu.ar'); 
-INSERT INTO person (FIRSTNAME, LASTNAME, EMAIL) VALUES ('Walter', 'Ureta', 'wureta@unlam.edu.ar');
-INSERT INTO person (FIRSTNAME, LASTNAME, EMAIL) VALUES ('Juan', 'Monteagudo', 'jmonteagudo@unlam.edu.ar');
-
-INSERT INTO user_ (USERNAME, PASSWORD, EMAIL) VALUES('hector', '1234', 'vhg_92@outlook.com');
-INSERT INTO user_ (USERNAME, PASSWORD, EMAIL) VALUES('martin', '1234', 'vhg_92@outlook.com');
-INSERT INTO user_ (USERNAME, PASSWORD, EMAIL) VALUES('agustin', '1234', 'vhg_92@outlook.com');
-INSERT INTO user_ (USERNAME, PASSWORD, EMAIL) VALUES('sergio', '1234', 'vhg_92@outlook.com');
-
-INSERT INTO user_ (USERNAME, PASSWORD, EMAIL, CONDITION) VALUES('victor', '1234', 'vhg_92@outlook.com', 'ADMIN_USER');
-
-INSERT INTO task (MODE, FIRSTNAME_TO_ADD, LASTNAME_TO_ADD, EMAIL_TO_ADD) VALUES ('ALTA', 'JUAN', 'LOPEZ', 'vic@hotmail.com');
-INSERT INTO task (MODE, FIRSTNAME_TO_ADD, LASTNAME_TO_ADD, EMAIL_TO_ADD, TARGET_REGISTER) VALUES ('BAJA', 'Juan', 'Monteagudo', 'jmonteagudo@unlam.edu.ar', 1);
-INSERT INTO task (MODE, FIRSTNAME_TO_ADD, LASTNAME_TO_ADD, EMAIL_TO_ADD) VALUES ('MODIFICACION', 'JUAN', 'LOPEZ', 'vic@hotmail.com');
-
---COMMIT;
+INSERT INTO users (name, surname, email, pasword) VALUES ('Cintia', 'Gioia', 'cgioia@unlam.edu.ar', '1234'); 
+INSERT INTO users (name, surname, email, pasword) VALUES ('Walter', 'Ureta', 'wureta@unlam.edu.ar', '1234');
+INSERT INTO users (name, surname, email, pasword) VALUES ('Juan', 'Monteagudo', 'jmonteagudo@unlam.edu.ar', '1234');
+INSERT INTO users (name, surname, email, pasword, is_admin) VALUES ('Agustin', 'Buzarquis', 'abuzarquis@unlam.edu.ar', '1234', 1);
